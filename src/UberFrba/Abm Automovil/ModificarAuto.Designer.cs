5c1afe55a4841069ecc02561923520def5ca4aa6
FMagarik <facu.emiliano@gmail.com>
2017-06-12 08:52:43 -0300

    Integración de modificaciones de Aye
    Se integra la modificación de autos, así como algunos store procedure adicionales

---
+++ src/UberFrba/Abm Automovil/ModificarAuto.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace UberFrba.Abm_Automovil
+{
+    partial class ModificarAuto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelEstado = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(285, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Buscar Auto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.botonBuscar);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.labelEstado);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(375, 236);
+            this.groupBox1.TabIndex = 40;
+            this.groupBox1.TabStop = false;
+            // 
+            // labelEstado
+            // 
+            this.labelEstado.AutoSize = true;
+            this.labelEstado.BackColor = System.Drawing.SystemColors.Control;
+            this.labelEstado.ForeColor = System.Drawing.Color.Red;
+            this.labelEstado.Location = new System.Drawing.Point(107, 16);
+            this.labelEstado.Name = "labelEstado";
+            this.labelEstado.Size = new System.Drawing.Size(51, 13);
+            this.labelEstado.TabIndex = 49;
+            this.labelEstado.Text = "<estado>";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(55, 13);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(32, 20);
+            this.textBox4.TabIndex = 48;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 47;
+            this.label6.Text = "Auto";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(55, 203);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 46;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "Turno";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(55, 160);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 44;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 43;
+            this.label3.Text = "Patente";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 42;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "Modelo";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(161, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 40;
+            this.button3.Text = "Cambiar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 37;
+            this.label5.Text = "Chofer";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(55, 46);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 40;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(55, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 30;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Marca";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(303, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 48;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(222, 300);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 47;
+            this.button4.Text = "Modificar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.botonModificar);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 300);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 49;
+            this.button5.Text = "Limpiar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(161, 10);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 50;
+            this.button6.Text = "Habilitar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.botonHabili);
+            // 
+            // ModificarAuto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 335);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "ModificarAuto";
+            this.Text = "Modificar Automovil";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label labelEstado;
+        private System.Windows.Forms.Button botonHabilitar;
+        private System.Windows.Forms.Button button6;
+    }
+}
---
+++ src/UberFrba/Abm Automovil/ModificarAuto.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace UberFrba.Abm_Automovil {
+    public partial class ModificarAuto : Form, ComunicacionForms
+    {
+        
+        
+        public ModificarAuto()
+        {
+            InitializeComponent();
+            LlenarCombos();
+            labelEstado.Text = "" ;
+        }
+        public void editarAuto(string id)
+        {
+            textBox4.Text = id;
+            SqlDataReader reader = Conexion.ejecutarQuery("select auto_habilitado from RUBIRA_SANTOS.AUTOMOVIL where auto_id  like '%" + textBox4.Text + "%'");
+            reader.Read();
+            string estadoAuto = (reader["auto_habilitado"].ToString());
+            reader.Close();
+            if (estadoAuto.Equals("True"))
+                labelEstado.Text = "HABILITADO";
+            else
+                labelEstado.Text = "INHABILITADO";
+           
+        }
+        public void editarCliente(string id)
+        {
+                
+          }
+        public void editar(string id)
+        {
+
+        }
+        public void guardarMarca(string marca)
+        {
+            comboBox1.Text = marca;
+
+        }
+        public void guardarPatente(string patente)
+        {
+            textBox2.Text = patente;
+
+        }
+        public void guardarModelo(string modelo)
+        {
+            textBox1.Text = modelo;
+
+        }
+        public void editarTurno(string turno)
+        {
+            comboBox2.Text = turno;
+         
+        }
+        public void editarChofer(string id)
+        {
+            textBox3.Text = id;
+        }
+        
+        private void LlenarCombos()
+        {
+            Funciones.llenarCombo_Marca(comboBox1);
+            Funciones.llenarCombo_Turno(comboBox2);
+        }
+        
+        private void botonBuscar(object sender, EventArgs e)
+        {
+            Abm_Automovil.BusquedaAuto buscar = new Abm_Automovil.BusquedaAuto();
+
+            buscar.Show(this);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void botonModificar(object sender, EventArgs e)
+        {
+            if( validaciones())
+            {
+                bool conex = Conexion.executeProcedure("ModificarAuto", Conexion.generarArgumentos("@auto", "@nuevaMarca","@modelo","@patente","@chofer","@turno"),
+                 textBox4.Text, comboBox1.Text,textBox1.Text,textBox2.Text,textBox3.Text,comboBox2.Text);
+                if (conex)
+                {
+                    MessageBox.Show("Auto modificado correctamente");
+                }
+            }
+        }
+        private bool validaciones()
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("El campo Marca no puede estar vacio");
+                return false;
+            }
+            if (comboBox2.SelectedIndex == -1)
+            {
+                MessageBox.Show("El campo Turno no puede estar vacio");
+                return false;
+            }
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("El campo Modelo no puede estar vacio");
+                return false;
+            }
+
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show("El campo Patente no puede estar vacio");
+                return false;
+            }
+
+            if (textBox3.Text == "")
+            {
+                MessageBox.Show("El campo Chofer no puede estar vacio");
+                return false;
+            }
+            return true;
+        }   
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Funciones.llenarCombo_Marca(comboBox1);
+            Funciones.llenarCombo_Turno(comboBox2);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            labelEstado.Text = "";
+
+        }
+
+        private void botonHabili(object sender, EventArgs e)
+        {
+            bool conex = Conexion.executeProcedure("habilitar_automovil", Conexion.generarArgumentos("@id"), textBox4.Text);
+            if (conex)
+            {
+                MessageBox.Show("Automovil habilitado correctamente");
+            }
+        }
+    }
+}
