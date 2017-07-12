b74f68d6ca42bead72f22cef58ad8d126da623f3
FMagarik <facu.emiliano@gmail.com>
2017-07-12 00:02:36 -0300

    Fix Creación de Pago

--- src/UberFrba/Rendicion Viajes/CrearPago.Designer.cs
+++ src/UberFrba/Rendicion Viajes/CrearPago.Designer.cs
@@ -142,6 +142,7 @@
             this.button4.TabIndex = 2;
             this.button4.Text = "Seleccionar";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label1
             // 
